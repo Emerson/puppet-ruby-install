f00478de7f311fcbe7ffa6ab0b1c13a1d1f5be38
Emerson Lackey <emerson.lackey@gmail.com>
2013-07-19 12:55:22 -0400

    full path instead of alias

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,34 +1,34 @@
 class ruby-install {
 
   # Create the source folder
-  file { '~/source':
+  file { '/root/source':
     ensure => 'directory',
   }->
 
   # Download Ruby
   exec { 'download_ruby':
     command => 'wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz',
-    cwd     => '~/source',
+    cwd     => '/root/source',
   }->
 
   exec { 'uncompress_ruby':
     command => 'tar -zxvf ~/source/ruby-2.0.0-p247.tar.gz',
-    cwd     => '~/source',
+    cwd     => '/root/source',
   }->
 
   exec { 'configure_ruby':
     command => 'configure',
-    cwd     => '~/source/ruby-2.0.0-p247',
+    cwd     => '/root/source/ruby-2.0.0-p247',
   }->
 
   exec { 'make_ruby':
     command => 'make',
-    cwd     => '~/source/ruby-2.0.0-p247',
+    cwd     => '/root/source/ruby-2.0.0-p247',
   }->
 
   exec { 'install_ruby':
     command => 'make install',
-    cwd     => '~/source/ruby-2.0.0-p247',
+    cwd     => '/root/source/ruby-2.0.0-p247',
   }
 
 }
